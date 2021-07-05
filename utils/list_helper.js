5ec807ed937ed87a48e9a5da6b24d5c260d504b5
thanatly <82842370+thanatly@users.noreply.github.com>
2021-07-05 12:01:52 +0300

    Commit for ex 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
-const dummy = (blogs) => {
-    return 1
-  }
+function amount(item){
+return item.likes;
+}
+
+function sum(prev, next){
+return prev + next;
+}
+
+const totalLikes = (blogs)  => {
+    return blogs.length === 0
+    ? 0
+    : blogs.map(amount).reduce(sum);
+}
   
   module.exports = {
-    dummy
+    totalLikes
   }
+
