bea67c5564a2ad6dee1cd5242120c039cd05681b
thanatly <82842370+thanatly@users.noreply.github.com>
2021-07-15 17:43:27 +0300

    Commit for ex 4.11, 4.12
    Add test_helper file and refactor

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,30 +2,15 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./test_helper')
 
-//Initialize the DB
 const Blog = require('../models/blog')
-const initialBlogs = [
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    id: "5a422aa71b54a676234d17f8"
-  },
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    id: "5a422a851b54a676234d17f7"
-  },
-]
+
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
@@ -39,7 +24,7 @@ test('blogs are returned as json', async () => {
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('a specific blog is within the returned blogs', async () => {
@@ -78,8 +63,11 @@ test('a valid blog can be added', async () => {
 
   const contents = response.body.map(r => r.title)
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(contents).toContain(
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+  const titles = blogsAtEnd.map(n => n.title)
+  expect(titles).toContain(
     'Canonical string reduction'
   )
 })
@@ -114,8 +102,8 @@ test('blog without title and url is not added', async () => {
   .send(incompleteBlog)
   .expect(400)
 
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(initialBlogs.length)
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 }, 100000)
 
 afterAll( async () => {
---
+++ tests/test_helper.js
@@ -0,0 +1,28 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    id: "5a422aa71b54a676234d17f8"
+  },
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    id: "5a422a851b54a676234d17f7"
+  },
+]
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({})
+    return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+    initialBlogs,
+    blogsInDb
+  }
