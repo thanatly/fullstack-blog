1ced2f2f6ba5b3dd41fec1fc11c911dad61da068
thanatly <82842370+thanatly@users.noreply.github.com>
2021-07-21 09:20:55 +0300

    Commit for ex 4.11 & 4.12
    Refactor promise chains into await/async

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+//make all router in async/ wait syntax  
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog
-      .find({})
-      .catch(error => next(error))
-    response.json(blogs)
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
   })
   
-blogsRouter.post('/', (request, response, next) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(savedBlog => {
-        response.json(savedBlog.toJSON())
-      })
-      .catch(error => next(error))
+// 4.13 delete
+// 4.14 update blog
+blogsRouter.post('/', async (request, response, next) => {
+    const body = request.body
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    })
+
+    try { 
+      const savedBlog = await blog.save()
+      response.json(savedBlog.toJSON())
+    } catch(exception) {
+      next(exception)
+    }
   })
 
 module.exports = blogsRouter
