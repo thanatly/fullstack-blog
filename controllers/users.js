e69ec4a06309ce50b3f58dd6f6db743d05633767
thanatly <82842370+thanatly@users.noreply.github.com>
2021-07-23 09:41:42 +0300

    Commit for ex 4.15
    Populate blog to user db

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,10 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
-    response.json(users)
+  const users = await User
+  .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+  
+  response.json(users)
 })  
 
 usersRouter.post('/', async (request, response) => {
